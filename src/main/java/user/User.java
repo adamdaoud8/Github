2c88257f2f6fe79851ce8bbbe384c05ecf7ad8af
User <adam-daoud@live.com>
2025-03-31 15:30:29 +0200

    Github

---
+++ src/main/java/user/User.java
@@ -0,0 +1,28 @@
+package user;
+
+public class User {
+
+    private String userName;
+    private String password;
+
+    public User(String userName, String password) {
+        this.userName = userName;
+        this.password = password;
+    }
+
+    public String getName() {
+        return userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setUserName(String newName) {
+        userName = newName;
+    }
+
+    public String getUserName() {
+        return "";
+    }
+}
---
+++ src/test/java/UserTest/Usertest.java
@@ -0,0 +1,39 @@
+package UserTest;
+
+import user.User;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class Usertest {
+
+    @Test
+    public void testCreateUser() {
+        User user = new User("Adam", "password123");
+    }
+
+    @Test
+    public void testGetUserName() {
+        User user = new User("Adam", "password123");
+        String expected = "Adam";
+        String actual = user.getName();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testGetPassword() {
+        User user = new User("Adam", "password123");
+        String expected = "password123";
+        String actual = user.getPassword();
+        assertEquals(expected, actual);
+        System.out.println("password123");
+    }
+    @Test
+    public void testSetUserName() {
+        User user = new User("Adam", "password123");
+        String expected = "Henry";
+        user.setUserName("Henry");
+        String actual = user.getUserName();
+        assertEquals(expected, actual);
+    }
+}
